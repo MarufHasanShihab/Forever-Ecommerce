fd0815af44bc8625c9147fd7c4f2629c6a78ae0d
Maruf Hasan Shihab <marufhasanshihab2@gmail.com>
2024-09-11 19:31:21 +0600

    added navbar

--- src/components/Laoyouts/MainLayout.jsx
+++ src/components/Laoyouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 
 const MainLayout = () => {
     return (
         <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+            <Navbar/>
             <Outlet/>
         </div>
     );
